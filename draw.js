fcdd33ae7ac62c12a14dc8459eff96c4b210e148
Gabriel Moraes <moraes.madm@gmail.com>
2018-01-18 22:40:53 -0200

    Collision check function
    Creates collision check function

--- draw.js
+++ draw.js
@@ -59,7 +59,14 @@ var createFood = function() {
 	}
 }
 
-
+var checkCollision = function(x, y, array) {
+	for(var i = 0; i < array.length; i++){
+		if (array[i].x === x && array[i].y === y) {
+			return true;
+		}
+	}
+	return false;
+}
 
 
 
