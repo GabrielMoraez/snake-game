a4d1f56448aeca58635d15381d569af117a7273b
Gabriel Moraes <moraes.madm@gmail.com>
2018-01-18 23:02:43 -0200

    More things added to draw.js
    added checkcollision's if, painted the canvas, added movement, added snake's growth

--- draw.js
+++ draw.js
@@ -68,6 +68,89 @@ var checkCollision = function(x, y, array) {
 	return false;
 }
 
+var paint = function() {
+	//Let's draw the space in wich the snake will move
+	ctx.fillStyle = 'lightGray';
+	ctx.fillRect(0, 0, w, h);
+
+	//Give it a border 
+	ctx.strokeStyle = 'black';
+	ctx.strokeRect(0, 0, w, h);
+
+	// Disable start button while you're playing
+	btn.setAttribute('disabled', true);
+
+	var snakeX = snake[0].x;
+	var snakeY = snake[0].y;
+
+	/*
+	Make the snake move.
+	Use a variable ('direction') to control the movement.
+	To move the snake, pop out the last element of the array and shift it on the top as first element.
+	*/
+
+	if (direction == 'right') {
+		snakeX ++;
+	} else if (direction == 'left') {
+		snakeX --;
+	} else if (direction == 'up') {
+		snakeY --;
+	} else if (direction == 'down') {
+		snakeY ++;
+	}
+
+	/* If the snake touches the canvas border or itself, it will die.
+	Therefore if x or y of an element of the snake don't fit inside the canvas, the game will be stopped.
+	If the checkCollision is true, it means that the snake has crashed on its body itself, then the game will be stopped.
+	*/
+	if (snakeX == -1 || snakeX == w / snakeSize || snakeY == -1 || snakeY == h / snakeSize || checkCollision(snakeX, snakeY, snake)) {
+		//Stop the game
+
+		//Make the start button enable again
+		btn.removeAttribute('disabled', true);
+
+		//Clean up the canvas
+		ctx.clearRect(0, 0, w, h);
+		gameloop = clearInterval(gameloop);
+		return;
+	}
+
+	// If the snake eats the food it becomes longer and this means that, in this case, you shouldn't pop out the last element of the array
+	if (snakeX == food.x && snakeY == food.y) {
+		//Create a new square instead of moving the tail
+		var tail = {
+			x: snakeX,
+			y: snakeY
+		};
+		score++;
+
+		//Create new food
+		createFood();
+	} else {
+
+		//Pop out the last cell
+		var tail = snake.pop();
+		tail.x = snakeX;
+		tail.y = snakeY;
+	}
+
+	// Puts the tail as the first cell
+	snake.unshift(tail);
+
+	// For each element of the array create a square using the bodySnake function we created before
+	for (var i = 0; i < snake.length; i++) {
+		bodySnake(snake[i].x, snake[i].y);
+	}
+
+	// Create food using the pizza function
+
+	pizza(food.x, food.y);
+
+	// Put the score text
+
+	scoreText();
+}
+
 
 
 
