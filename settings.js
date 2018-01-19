bade4e334ab438899fdf990aebe099dc06b6db14
Gabriel Moraes <moraes.madm@gmail.com>
2018-01-18 22:26:12 -0200

    Fixing some code errors
    Created draw.js file and transported most of the code from settings.js to it

--- settings.js
+++ settings.js
@@ -10,30 +10,3 @@ var snake;
 var snakeSize = 10;
 var food;
 
-
-
-var drawModule = (function() {
-	var bodySnake = function (x, y) {
-		// This is the single square
-		ctx.fillStyle = 'green';
-		ctx.fillRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
-		//This is the border of the square
-		ctx.strokeStyle = 'darkgreen';
-		ctx.strokeRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
-	}
-
-	var pizza = function(x, y) {
-		//This is the border of the pizza
-		ctx.fillStyle = 'yellow';
-		ctx.fillRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
-		//This is the single square
-		ctz.fillStyle = 'red';
-		ctx.fillRect(x*snakeSize+1, y*snakeSize+1, snakeSize-2, snakeSize-2);
-	}
-
-	var scoreText = function() {
-		//How many pizzas did the snake ate
-		var score_text = "Score: " + score;
-		ctx.fillStyle = 'blue';
-		ctx.fllText(score_text, 145, h-5);
-	}
